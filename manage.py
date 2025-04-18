8eb1e57ccfc3426590013cb266f5a60d7a5a1167
Tabassum Maisha <tabassummaisha17@gmail.com>
2025-04-18 11:41:02 -0700

    Add files via upload

---
+++ manage.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
+import os
+import sys
+
+
+def main():
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'E_commerce.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
+
+# if __name__ == "__main__":
+#     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "E_commerce.settings")
+#     try:
+#         from django.core.management import execute_from_command_line
+#     except ImportError as exc:
+#         raise ImportError(
+#             "Couldn't import Django. Are you sure it's installed and "
+#             "available on your PYTHONPATH environment variable? Did you "
+#             "forget to activate a virtual environment?"
+#         ) from exc
+#     # Bind to the port from the environment variable, defaulting to 8000
+#     port = os.getenv('PORT', '8000')
+#     execute_from_command_line(['manage.py', 'runserver', f'0.0.0.0:{port}'])
